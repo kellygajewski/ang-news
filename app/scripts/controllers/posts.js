013c783461fbd3474fb77175876d1f5b6436561e
Kelly Gajewski <kellyagajewski@gmail.com>
2014-07-27 09:19:36 -0700

    using the post service

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -4,14 +4,21 @@ app.factory('Post', function($resource){
 	return $resource('https:https://amber-fire-7630.firebaseio.com/posts/:id.json');
 });
 
-app.controller('PostsCtrl', function ($scope) {
-	$scope.posts = [];
+app.controller('PostsCtrl', function ($scope, Post) { //Post is service created in post.js
+	//$scope.posts = [];
+	$scope.posts = Post.get(); //sends get request to resource url without any parameters
 	$scope.post = {url: 'http://', title: ''};
 	
+	
 	$scope.submitPost = function() {
-		$scope.posts.push($scope.post);
-		$scope.post = {url: 'http://', title: ''};
+		// $scope.posts.push($scope.post);
+		// $scope.post = {url: 'http://', title: ''};
+		Post.save($scope.post, function (ref) {
+			$scope.posts[ref.name] = $scope.post;
+			$scope.post = {url: 'http://', title: ''};
+		});
 	};
+	//Send a POST request to our posts endpoint with our post object. The save function takes a second parameter as a function for our view to update only if the request is successful  (success callback). In the callback, we get the object returned from the server as a parameter. Firebase will return the ID in an object that's a reference to the saved post in this format: {name: (postId here)} so we can create a callback to set the item on $scope.posts
 
 	$scope.deletePost = function(index) {
 		$scope.posts.splice(index, 1);
